c674f0d2a73e07f721c2aa4dc7a5ae94282fa279
Nathaniel Truitt <ntruitt@Nathaniels-MBP.localdomain>
2022-08-31 09:57:47 -0500

    created bookmark app

---
+++ Class-5/app.js
@@ -0,0 +1,55 @@
+// Grab elements from the DOM
+const overlay = document.querySelector(".overlay");
+const input = document.querySelector("#url");
+const button = document.querySelector("button");
+const linkContainer = document.querySelector(".link-container");
+const apiURL = "https://opengraph.io/api/1.1/site";
+// API token to OpenGraph API.
+const apiKey = data.token;
+let savedItems = localStorage.getItem("linksData") || [];
+if (typeof savedItems === "string") {
+  savedItems = JSON.parse(savedItems);
+  let innerHTML = "";
+  for (item of savedItems) {
+    innerHTML += item;
+  }
+  linkContainer.innerHTML = innerHTML;
+}
+
+// functions
+function toggleOverlay(event) {
+  if (event.type === "focusin") {
+    overlay.style.opacity = 0.5;
+  } else {
+    overlay.style.opacity = 0;
+  }
+}
+
+function createItem() {
+  fetch(
+    `${apiURL}/${encodeURIComponent(
+      input.value
+    )}?accept_lang=auto&app_id=${apiKey}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const newItem = `<div class="link-item"><img src="${data.hybridGraph.image}" alt="${data.hybridGraph.title}" /><h2><a href="${input.value}">${data.hybridGraph.title}</a></h2></div>`;
+      savedItems.push(newItem);
+      saveItems();
+      linkContainer.innerHTML = linkContainer.innerHTML + newItem;
+    });
+
+  //   const newItem = `<div class="link-item"><img src="" alt="" /><h2>${input.value}</h2></div>`;
+  // savedItems.push(newItem);
+  // saveItems();
+  // linkContainer.innerHTML = linkContainer.innerHTML + newItem;
+}
+
+function saveItems() {
+  localStorage.setItem("linksData", JSON.stringify(savedItems));
+}
+
+// event listeners
+input.addEventListener("focusin", toggleOverlay);
+input.addEventListener("focusout", toggleOverlay);
+button.addEventListener("click", createItem);
